fdc5d2324221f7b759fccc1c3dbf2c993dda1052
YehudaNevo <ynevo1948@gmail.com>
2024-01-18 15:58:15 +0200

    finish uart

---
+++ 1_UART_DRIVER/Inc/uart.h
@@ -0,0 +1,10 @@
+#ifndef __UART_H__
+#define __UART_H__
+
+void uart_tx_init(void);
+void uart_write(int ch);
+
+
+
+#endif
+
--- 1_UART_DRIVER/Src/main.c
+++ 1_UART_DRIVER/Src/main.c
@@ -1,17 +1,44 @@
+#include <stdio.h>
 #include "led.h"
+#include "uart.h"
 
 int main(void)
 {
 
 
-	led_init();
+//	int _write(int file, char *ptr, int len) {
+//	    int i;
+//	    for (i = 0; i < len; i++) {
+//	        uart_write(ptr[i]);
+//	    }
+//	    return len;
+//	}
+
+
+//	__io_putchar__('A'); // Should send the character 'A' over UART
+
+
+
+//	led_init();
+
+
+	uart_tx_init();
+
+
+//debuging method
+//const char *str = "hello from stm32f411\n";
+//
+//    while (*str) {
+//        uart_write(*str++);
+//    }
+
+
+//    setbuf(stdout, NULL);
 
 	while(1){
 
-		led_on();
-		for (int i = 0 ; i < 900000 ; i++){}
-		led_off();
-		for (int i = 0 ; i < 900000 ; i++){}
+		printf("hello from stm32f411 uart\n\r");
+	    for (int i = 0; i < 1000000; i++) {}
 
 
 	}
